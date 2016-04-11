0f32bb20d241406c39f99fe2430b17a2ff46218e
Alekh <alekh@letsalign.com>
2016-04-11 19:26:33 -0400

    Added modals and ghost buttons

--- js/function.js
+++ js/function.js
@@ -3,5 +3,17 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+function pictureChangeBlack()
+    {
+          document.getElementById("theImage").src="./images/black1.jpg";
+    }
 
+function pictureChangeRed()
+    {
+          document.getElementById("theImage").src="./images/red1.jpg";
+    }
 
+function pictureChangeBlue()
+    {
+          document.getElementById("theImage").src="./images/blue1.jpg";
+    }
